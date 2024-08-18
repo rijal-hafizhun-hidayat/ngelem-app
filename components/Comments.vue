0930d460d68131ba15d93f4e750257bdda4a7e64
unknown <rijal.1344@gmail.com>
2024-08-18 10:06:03 +0700

    component: add comment in post component

---
+++ components/Comments.vue
@@ -0,0 +1,8 @@
+<script setup>
+const model = defineModel();
+</script>
+<template>
+  <div class="my-auto">
+    <p>ini komentar</p>
+  </div>
+</template>
---
+++ components/LikeShareCount.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="space-x-1">
+    <span>60</span>
+    <span>
+      <font-awesome icon="fa-comment" />
+    </span>
+  </div>
+  <div class="space-x-1">
+    <span>50</span>
+    <span>
+      <font-awesome icon="fa-share" />
+    </span>
+  </div>
+</template>
